b43dec8e855f014638e3c61d9b15e5527b2890f3
Rafael Santana <rgs1990419@gmail.com>
2021-02-11 10:13:30 -0300

    Configuração react-router-dom

---
+++ sistema_de_chamados/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/cadastro" component={Cadastro} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
