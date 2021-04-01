65319ff47bba86b97789df31742752848e8097c3
AlexGimenes2001 <80415019+AlexGimenes2001@users.noreply.github.com>
2021-04-01 15:38:06 -0300

    Login Social

--- sistema_de_chamados/src/pages/Login/social.js
+++ sistema_de_chamados/src/pages/Login/social.js
@@ -31,7 +31,7 @@ function Social() {
     return (
         <div>
             <GoogleLogin
-                clientId="499021820618-c4dttfcpvpne9unmojlb6n3idbk3rr3l.apps.googleusercontent.com" 
+                clientId="499021820618-ljr020aiiqebrpj361bf2tf7uaprk9lb.apps.googleusercontent.com" 
                 buttonText="Sign in"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
